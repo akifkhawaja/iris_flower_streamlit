da2e012bd8bc1f6ad6ae024286c76146b79b4ccd
Akif Khawaja <61511915+akifkhawaja@users.noreply.github.com>
2023-09-18 02:15:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
 import pickle
 
 st.title("Iris Flower Classification")
